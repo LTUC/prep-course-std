bbc2a5dd7e411b213a8bafd6b3d2de2e23ac226b
Ammarhr <hariry49@gmail.com>
2022-01-23 15:29:37 +0200

    delete skip in the tests

--- Day09/ProblemSolving/Challenge09/Challenge09.test.js
+++ Day09/ProblemSolving/Challenge09/Challenge09.test.js
@@ -2,8 +2,7 @@
 
 const { arrInc, roundDecimals, employeesBonus, mostExpensive } = require('./Challenge09');
 
-const data = [
-    {
+const data = [{
         name: "Robert",
         section: "Transport",
         workHours: 8,
@@ -29,8 +28,7 @@ const data = [
     }
 ];
 
-const res = [
-    {
+const res = [{
         name: "Robert",
         section: "Transport",
         workHours: 8,
@@ -56,8 +54,7 @@ const res = [
     }
 ];
 
-const data2 = [
-    {
+const data2 = [{
         name: "Jack",
         section: "Coding",
         workHours: 6,
@@ -71,8 +68,7 @@ const data2 = [
     }
 ];
 
-const res2 = [
-    {
+const res2 = [{
         name: "Jack",
         section: "Coding",
         workHours: 6,
@@ -86,7 +82,7 @@ const res2 = [
     }
 ];
 
-describe.skip('Challenge09', () => {
+describe('Challenge09', () => {
     test('Array-Increment', () => {
         expect(arrInc([5, 15, 3, 10, 32])).toStrictEqual([15, 25, 13, 20, 42]);
         expect(arrInc([6, 0, -10, 33])).toStrictEqual([16, 10, 0, 43]);
@@ -94,15 +90,15 @@ describe.skip('Challenge09', () => {
 
 })
 
-describe.skip('Challenge09', () => {
+describe('Challenge09', () => {
     test('Round-Decimals', () => {
-        expect(roundDecimals([5.4, 5.5 ,6.7, 6.8])).toStrictEqual([5, 6, 7, 7]);
+        expect(roundDecimals([5.4, 5.5, 6.7, 6.8])).toStrictEqual([5, 6, 7, 7]);
         expect(roundDecimals([6.1, 2.3, 5, 2.9])).toStrictEqual([6, 2, 5, 3]);
     })
 
 })
 
-describe.skip('Challenge09', () => {
+describe('Challenge09', () => {
     test('Employees-Bonus', () => {
         expect(employeesBonus(data)).toStrictEqual(res);
         expect(employeesBonus(data2)).toStrictEqual(res2);
@@ -110,10 +106,10 @@ describe.skip('Challenge09', () => {
 
 })
 
-describe.skip('Challenge10', () => {
+describe('Challenge10', () => {
     test('Most-Expensive', () => {
-        expect(mostExpensive(200,[35, 15, 75, 180, 150, 50], [5, 150, 35, 120, 75, 50, 100])).toStrictEqual(200);
-        expect(mostExpensive(150,[35, 15, 75], [5, 150, 100])).toStrictEqual(135);
+        expect(mostExpensive(200, [35, 15, 75, 180, 150, 50], [5, 150, 35, 120, 75, 50, 100])).toStrictEqual(200);
+        expect(mostExpensive(150, [35, 15, 75], [5, 150, 100])).toStrictEqual(135);
     })
 
 })
