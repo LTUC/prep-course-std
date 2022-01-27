d2559e0f71cf6ca024143871c30f2bdc4ab1e19b
Eng-Ahmad-Almohammad <almohammedahmed2@gmail.com>
2022-01-27 15:12:37 +0200

    Fix a typo

--- Day10/Task/recursion/recursion.test.js
+++ Day10/Task/recursion/recursion.test.js
@@ -30,7 +30,9 @@ function fibonacci(n){
     return;
 }
 
-/* The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+/* Optional 
+
+The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
 
 By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
 
@@ -44,7 +46,7 @@ Given n and k, return the kth permutation sequence.
 
 Example: 
 Input: n = 3, k = 3
-Output: "213" */
+Output: ["123", "132", "213", "231", "312", "321"] */
 
 function permutations(n,k){
     let arr = [];
